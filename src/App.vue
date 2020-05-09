794587b850b323c511e26df5348398237b85bccc
郝斌 <7350648+hao_bin@user.noreply.gitee.com>
2020-05-09 10:53:03 +0800

    项目初始化-导入Van组件库

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,17 @@
 <template>
   <div id="app">
     <h1>头条啊</h1>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
     <router-view/>
   </div>
 </template>
 <script>
 export default {
-  name: "App"
+  name: 'App'
 }
 </script>
 <style lang="less"></style>
--- src/main.js
+++ src/main.js
@@ -2,7 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+import Vant from 'vant'
+import 'vant/lib/index.css'
+Vue.use(Vant)
 Vue.config.productionTip = false
 //  创建Vue根实例 将 router strore 配置到
 new Vue({
