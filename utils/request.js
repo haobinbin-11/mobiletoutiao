01fc3c74ee2bd39f32217ce5e20cddb6465c2f44
郝斌 <7350648+hao_bin@user.noreply.gitee.com>
2020-05-09 23:38:02 +0800

    项目初始化-封装请求模块

---
+++ utils/request.js
@@ -0,0 +1,13 @@
+// 请求模块
+import axios from 'axios'
+
+const requset = axios.create({
+    baseURL: 'http://ttapi.research.itcast.cn' // 基础路径
+})
+
+// 请求拦截器
+
+// 响应拦截器
+
+// 导出
+export default requset
