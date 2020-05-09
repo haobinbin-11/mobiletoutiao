28aa63a3d933c8ed1cde60f9dd5f6bc694f1853c
郝斌 <7350648+hao_bin@user.noreply.gitee.com>
2020-05-09 10:59:54 +0800

    项目初始化-移动端配置REM适配-设置REM基准值

--- src/main.js
+++ src/main.js
@@ -2,8 +2,18 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+
+//加载全局样式
+import './styles/index.css'
+
+// 加载 Vant 组件库
 import Vant from 'vant'
+
+// 加载 Vant 组件库样式
 import 'vant/lib/index.css'
+
+// 手动设置 REM 基准值 (html 标签字体大小)
+import 'amfe-flexible'
 Vue.use(Vant)
 Vue.config.productionTip = false
 //  创建Vue根实例 将 router strore 配置到
