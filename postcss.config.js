fee26bbb758bafb92c240c6e47a98a33c311f930
郝斌 <7350648+hao_bin@user.noreply.gitee.com>
2020-05-09 11:39:35 +0800

    项目初始化-移动端配置REM适配-将px转换为rem

---
+++ postcss.config.js
@@ -0,0 +1,12 @@
+// PostCSS 的配置文件
+module.exports = {
+    plugins: {
+      autoprefixer: {
+        browsers: ['Android >= 4.0', 'iOS >= 8'],
+      },
+      'postcss-pxtorem': {
+        rootValue: 37.5,
+        propList: ['*'],
+      },
+    },
+  };
