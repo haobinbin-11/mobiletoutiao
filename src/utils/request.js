e0d52c14b46f80cb9d6e2fb22553f5b63dfef3d3
郝斌 <7350648+hao_bin@user.noreply.gitee.com>
2020-05-10 01:57:34 +0800

    h

--- src/utils/request.js
+++ src/utils/request.js
@@ -2,7 +2,7 @@
 import axios from 'axios'
 
 const requset = axios.create({
-    baseURL: 'http://ttapi.research.itcast.cn' // 基础路径
+  baseURL: 'http://ttapi.research.itcast.cn' // 基础路径
 })
 
 // 请求拦截器
