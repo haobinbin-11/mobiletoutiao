39d6a89546ad366bba988fa9578ca713b3dbe27d
郝斌 <7350648+hao_bin@user.noreply.gitee.com>
2020-05-10 01:26:47 +0800

    登录注册-实现基本登录功能

---
+++ src/api/user.js
@@ -0,0 +1,11 @@
+// 用户相关请求模块
+import request from '@/utils/request'
+
+// 登录注册
+export const login = data => {
+  return request({
+    method: 'POST',
+    url: '/app/v1_0/authorizations',
+    data
+  })
+}
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -28,13 +28,19 @@
       </van-field>
     </van-cell-group>
     <div class="login-btn-wrap">
-      <van-button class="login-btn" type="info" block >登录</van-button>
+      <van-button
+        class="login-btn"
+        type="info"
+        block
+        @click="onLogin"
+      >登录</van-button>
     </div>
     <!-- /登录表单 -->
   </div>
 </template>
 
 <script>
+import { login } from '@/api/user'
 export default {
   name: 'LoginIndex',
   components: {},
@@ -51,7 +57,17 @@ export default {
   watch: {},
   created () {},
   mounted () {},
-  methods: {}
+  methods: {
+    async onLogin () {
+      try {
+        const res = await login(this.user)
+        console.log(res)
+      } catch (err) {
+        console.log(err)
+        console.log('登录失败', err)
+      }
+    }
+  }
 }
 </script>
 <style scoped lang="less">
