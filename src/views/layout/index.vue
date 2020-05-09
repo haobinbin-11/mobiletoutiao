01124ee2953157a27a7b8aff7feb538aa3b496e4
郝斌 <7350648+hao_bin@user.noreply.gitee.com>
2020-05-10 00:10:35 +0800

    登录注册-页面布局-结构

--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -5,7 +5,7 @@
       <!-- /子路由出口 -->
 
       <!-- 底部导航栏 -->
-      <van-tabbar v-model="active" route="">
+      <van-tabbar route="">
         <van-tabbar-item icon="home-o" to="/">首页</van-tabbar-item>
         <van-tabbar-item icon="comment-o" to="/qa">问答</van-tabbar-item>
         <van-tabbar-item icon="video-o" to="/video">视频</van-tabbar-item>
