57ea87cad10753f86a36bdb1bc46df5d0f00b8ea
郝斌 <7350648+hao_bin@user.noreply.gitee.com>
2020-05-10 15:28:24 +0800

    登录注册-调整全局样式加载的顺序

--- src/main.js
+++ src/main.js
@@ -2,18 +2,17 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
-// 加载全局样式
-import './styles/index.css'
+// 加载 Vant 组件库样式
+import 'vant/lib/index.css'
 
 // 加载 Vant 组件库
 import Vant from 'vant'
 
-// 加载 Vant 组件库样式
-import 'vant/lib/index.css'
-
 // 手动设置 REM 基准值 (html 标签字体大小)
 import 'amfe-flexible'
+
+// 加载全局样式(放在最后方便覆盖第三方包样式)
+import './styles/index.css'
 Vue.use(Vant)
 Vue.config.productionTip = false
 //  创建Vue根实例 将 router strore 配置到
