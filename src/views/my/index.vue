92cb969a48f017cd255fe15207d88f7bd754a15b
郝斌 <7350648+hao_bin@user.noreply.gitee.com>
2020-05-11 17:57:43 +0800

    页面布局-头部用户信息

--- src/views/my/index.vue
+++ src/views/my/index.vue
@@ -1,5 +1,31 @@
 <template>
-  <div class="my-container">我的页面</div>
+  <div class="my-container">
+    <!-- <div class="user-info">
+      <div class="base-info"></div>
+      <div class="data-info"></div>
+    </div> -->
+    <van-cell-group>
+      <van-cell
+        center
+        title="单元格"
+        value="内容"
+      >
+        <van-image
+          slot="icon"
+          width="50"
+          height="50"
+          round
+          fit="cover"
+          src="https://img.yzcdn.cn/vant/cat.jpeg"
+        />
+        <div slot="title">昵称</div>
+        <van-buttton
+          size="small"
+          round
+        >编辑资料</van-buttton>
+      </van-cell>
+    </van-cell-group>
+  </div>
 </template>
 
 <script>
