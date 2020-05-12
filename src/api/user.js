61f4fa67e4299dfcb476063bbe76285fc28ead66
郝斌 <7350648+hao_bin@user.noreply.gitee.com>
2020-05-13 01:26:38 +0800

    我的-统一设置token

--- src/api/user.js
+++ src/api/user.js
@@ -1,6 +1,6 @@
 // 用户相关请求模块
 import request from '@/utils/request'
-import store from '@/store'
+// import store from '@/store'
 // 登录注册
 export const login = data => {
   return request({
@@ -20,9 +20,9 @@ export const sendSms = mobile => {
 export const getCurrentUser = () => {
   return request({
     method: 'GET',
-    url: '/app/v1_0/user',
-    headers: {
-      Authorization: `Bearer ${store.state.user.token}`
-    }
+    url: '/app/v1_0/user'
+    // headers: {
+    //   Authorization: `Bearer ${store.state.user.token}`
+    // }
   })
 }
--- src/utils/request.js
+++ src/utils/request.js
@@ -1,11 +1,20 @@
 // 请求模块
 import axios from 'axios'
-
+import store from '@/store/'
 const requset = axios.create({
   baseURL: 'http://ttapi.research.itcast.cn' // 基础路径
 })
 
 // 请求拦截器
+requset.interceptors.request.use(function (config) {
+  const { user } = store.state
+  if (user) {
+    config.headers.Authorization = `Bearer ${user.token}`
+  }
+  return config
+}, function (error) {
+  return Promise.reject(error)
+})
 
 // 响应拦截器
 
