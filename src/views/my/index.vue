ab1c4fdde672be2a0216ff5d075d47693dbc075f
郝斌 <7350648+hao_bin@user.noreply.gitee.com>
2020-05-12 23:41:48 +0800

    我的-页面布局完成

--- src/views/my/index.vue
+++ src/views/my/index.vue
@@ -4,23 +4,25 @@
       <div class="base-info"></div>
       <div class="data-info"></div>
     </div> -->
-    <van-cell-group>
+    <van-cell-group class="header">
       <!-- 头部用户信息 -->
       <van-cell
+        class="co"
         center
         title="单元格"
         value="内容"
+        :border="false"
       >
         <van-image
+          class="avator"
           slot="icon"
-          width="50"
-          height="50"
           round
           fit="cover"
           src="https://img.yzcdn.cn/vant/cat.jpeg"
         />
-        <div slot="title">昵称</div>
+        <div class="name" slot="title">昵称</div>
         <van-buttton
+          class="update-btn"
           size="small"
           round
         >编辑资料</van-buttton>
@@ -28,49 +30,52 @@
       <!-- /头部用户信息 -->
 
       <!-- 头部用户数据 -->
-      <van-grid>
-        <van-grid-item >
-          <div slot="text">
-            <div slot="span">123</div>
-            <div slot="text">头条</div>
+      <van-grid :border="false" class="shuju">
+        <van-grid-item class="shuju-item">
+          <div slot="text" class="text-wrap">
+            <div class="count">123</div>
+            <div class="text">头条</div>
           </div>
         </van-grid-item>
-        <van-grid-item >
-          <div slot="text">
-            <div slot="span">123</div>
-            <div slot="text">关注</div>
+        <van-grid-item class="shuju-item">
+          <div slot="text" class="text-wrap">
+            <div class="count">123</div>
+            <div class="text">关注</div>
           </div>
         </van-grid-item>
-        <van-grid-item >
-          <div slot="text">
-            <div slot="span">123</div>
-            <div slot="text">粉丝</div>
+        <van-grid-item class="shuju-item">
+          <div slot="text" class="text-wrap">
+            <div class="count">123</div>
+            <div class="text">粉丝</div>
           </div>
         </van-grid-item>
-        <van-grid-item >
-          <div slot="text">
-            <div slot="span">123</div>
-            <div slot="text">获赞</div>
+        <van-grid-item class="shuju-item">
+          <div slot="text" class="text-wrap">
+            <div class="count">123</div>
+            <div class="text">获赞</div>
           </div>
         </van-grid-item>
       </van-grid>
       <!-- /头部用户信息 -->
     </van-cell-group>
       <!-- 宫格导航 -->
-      <van-grid :column-num="2">
+      <van-grid class="nav-grid" :column-num="2">
         <van-grid-item
+          class="nav-grid-item"
           icon-prefix="hh"
           icon="shoucang"
-          text="文字"
+          text="收藏"
         />
         <van-grid-item
+          class="nav-grid-item"
           icon-prefix="hh"
           icon="lishi"
-          text="文字"
+          text="历史"
         />
       </van-grid>
       <!-- /宫格导航 -->
       <van-cell
+        class="cell-message"
         title="消息通知"
         is-link
         to="/"
@@ -80,7 +85,11 @@
         is-link
         to="/"
       />
-      <van-cell title="退出登录" />
+      <van-cell
+        class="jz"
+        title="退出登录"
+        center
+      />
   </div>
 </template>
 
@@ -99,4 +108,87 @@ export default {
   methods: {}
 }
 </script>
-<style scoped lang="less"></style>
+<style scoped lang="less">
+.my-container {
+  .header {
+    background: url("./1.png") no-repeat;
+    background-size: cover;
+    .co {
+      height: 115px;
+      box-sizing: border-box;
+      padding-top: 38px;
+      padding-bottom: 11px;
+      background-color: unset;
+      .avator {
+        width: 66px;
+        height: 66px;
+        border: 0.5px solid #ffffff;
+        box-sizing: border-box;
+        margin-right: 11px;
+      }
+      .naem {
+        font-size: 15px;
+        color: white;
+      }
+      .update-btn {
+        height: 7px;
+        background-color: #ffff;
+        padding: 3px;
+        font-size: 10px;
+        color: #666666;
+        border-radius: 20px;
+      }
+    }
+    .shuju {
+      .shuju-item {
+        height: 65px;
+        color: white;
+        .text-wrap {
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+          .count {
+            font-size: 18px;
+          }
+          .text {
+            font-size: 11px;
+          }
+        }
+      }
+    }
+    /deep/ .van-grid-item__content {
+      background-color: unset;
+    }
+  }
+  /deep/.nav-grid {
+    .nav-grid-item {
+      height: 70px;
+      .hh {
+        font-size: 22px;
+      }
+      .hh-shoucang {
+        color: #eb5253;
+      }
+      .hh-lishi {
+        color: #ff9d1d;
+      }
+      .van-grid-item__text {
+        font-size: 14px;
+        color: #333333;
+      }
+    }
+  }
+}
+.jz {
+  text-align: center;
+  margin-top: 5px;
+  color: #d86262;
+}
+.cell-message {
+  margin-top: 5px;
+}
+.c {
+  background: none !important;
+}
+</style>
