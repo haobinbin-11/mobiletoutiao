30a4f57bce8f508afd4c1e4419700148879640ab
haobinbin-11 <haobinbin-11@users.noreply.github.com>
2020-05-13 16:42:05 +0000

    Automated deployment: Wed May 13 16:42:05 UTC 2020 08ff3ec4aaa5abdb1465ac5c3521d7450c1d7c53

--- js/chunk-2d0a2e01.a43d3e0a.js
+++ js/chunk-2d0a2e01.a43d3e0a.js
@@ -1,2 +1,2 @@
 (window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0a2e01"],{"007b":function(n,e,t){"use strict";t.r(e);var c=function(){var n=this,e=n.$createElement,t=n._self._c||e;return t("div",{staticClass:"video-container"},[n._v("视频界面")])},o=[],a={name:"VideoIndex",components:{},props:{},data:function(){return{}},computed:{},watch:{},created:function(){},mounted:function(){},methods:{}},u=a,i=t("2877"),s=Object(i["a"])(u,c,o,!1,null,"773cf169",null);e["default"]=s.exports}}]);
-//# sourceMappingURL=chunk-2d0a2e01.eefdf949.js.map
+//# sourceMappingURL=chunk-2d0a2e01.a43d3e0a.js.map
