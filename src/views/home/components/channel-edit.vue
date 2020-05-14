e9f25938ef913ac8f1b953c748b88d3a2d3f5e2c
郝斌 <7350648+hao_bin@user.noreply.gitee.com>
2020-05-15 00:53:23 +0800

    频道编辑-页面布局

--- src/views/home/components/channel-edit.vue
+++ src/views/home/components/channel-edit.vue
@@ -1,5 +1,45 @@
 <template>
-  <div class="chanbel-eadit">频道编辑</div>
+  <div class="chanbel-eadit">
+    <!-- 我的频道 -->
+    <van-cell center :border="false">
+      <div
+        slot="title"
+        class="channel-title"
+      >我的频道</div>
+      <van-button
+        type="danger"
+        round
+        plain
+        size="mini"
+      >编辑</van-button>
+    </van-cell>
+    <van-grid :gutter="10">
+      <van-grid-item
+        class="grid-item"
+        v-for="value in 80"
+        :key="value"
+        text="文字"
+      />
+    </van-grid>
+    <!-- /我的频道 -->
+
+    <!-- 频道推荐 -->
+    <van-cell center :border="false">
+      <div
+        slot="title"
+        class="channel-title"
+      >频道推荐</div>
+    </van-cell>
+    <van-grid :gutter="10">
+      <van-grid-item
+        class="grid-item"
+        v-for="value in 8"
+        :key="value"
+        text="文字"
+      />
+    </van-grid>
+    <!-- /频道推荐 -->
+  </div>
 </template>
 
 <script>
@@ -17,4 +57,23 @@ export default {
   methods: {}
 }
 </script>
-<style scoped lang="less"></style>
+<style scoped lang="less">
+.chanbel-eadit {
+    padding-top: 55px;
+    .channel-title {
+        font-size: 16px;
+        color: #333333;
+    }
+    .grid-item {
+        width: 80px;
+        height: 43px;
+        /deep/ .van-grid-item__content{
+            background-color: #f4f5f6;
+            /deep/ .van-grid-item__text {
+                font-size: 14px;
+                color: #222;
+            }
+        }
+    }
+}
+</style>
