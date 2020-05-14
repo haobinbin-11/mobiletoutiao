bc1c65ddc4c87e572fc17dbbee8f17b2f89c6bd2
郝斌 <7350648+hao_bin@user.noreply.gitee.com>
2020-05-15 00:28:23 +0800

    频道编辑-处理汉堡按钮

---
+++ src/views/home/components/channel-edit.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="chanbel-eadit">频道编辑</div>
+</template>
+
+<script>
+export default {
+  name: 'ChannelEdit',
+  components: {},
+  props: {},
+  data () {
+    return {}
+  },
+  computed: {},
+  watch: {},
+  created () {},
+  mounted () {},
+  methods: {}
+}
+</script>
+<style scoped lang="less"></style>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -26,24 +26,47 @@
         <article-list :channel="channel" />
         <!-- /文章列表 -->
       </van-tab>
+      <!-- 添加占位元素 -->
+      <div slot="nav-right" style="width:33px; flex-shrink: 0;"></div>
+      <div slot="nav-right">
+        <van-icon
+          name="wap-nav"
+          class="wap-nav-warp"
+          @click="isChannelEditShow = true"
+        ></van-icon>
+      </div>
     </van-tabs>
     <!-- /文章频道列表 -->
+    <van-popup
+      v-model="isChannelEditShow"
+      position="bottom"
+      class="channel-edit-popup"
+      close-icon-position="top-left"
+      get-container="body"
+      style="height: 100%"
+      closeable
+    >
+      <channel-edit></channel-edit>
+    </van-popup>
   </div>
 </template>
 
 <script>
 import { getUserChannels } from '@/api/user'
 import ArticleList from './components/article-list'
+import ChannelEdit from './components/channel-edit'
 export default {
   name: 'HomeIndex',
   components: {
-    ArticleList
+    ArticleList,
+    ChannelEdit
   },
   props: {},
   data () {
     return {
       active: 0, // 控制被激活的标签
-      channels: [] // 频道列表
+      channels: [], // 频道列表
+      isChannelEditShow: true // 控制弹出层的显示状态
     }
   },
   computed: {},
@@ -88,6 +111,31 @@ export default {
       height: 3px;
       background-color: #3296fa;
       bottom: 20px;
+    }
+  }
+  .wap-nav-warp {
+    position: fixed;
+    right: 0;
+    width: 33px;
+    height: 43px;
+    line-height: 43px;
+    background-color: #ffffff;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    opacity: .9;
+    .van-icon {
+      font-size: 25px;
+    }
+    &::before {
+      display: block;
+      height: 43px;
+      background: url("./line.png") no-repeat;
+      background-size: contain;
+      position: absolute;
+      margin-right: -2px;
+      left: 0;
+      top: 0;
     }
   }
 }
